13529c2a65940aa02d5fe3a6d9b00644850c3cc0
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-23 18:42:03 -0300

    TextField and Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,29 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green,
         actions: [IconButton(icon: Icon(Icons.refresh), onPressed: () {})],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (Kg)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (Cm)",
+                labelStyle: TextStyle(color: Colors.green)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.green, fontSize: 25.0),
+          )
+        ],
+      ),
     );
   }
 }
